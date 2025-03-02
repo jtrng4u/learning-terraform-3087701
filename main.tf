fc6ae4840aa9a170c0d9625859b6076d90924432
jtrng4u <jtrng4u@gmail.com>
2025-03-02 09:16:02 -0600

    update alb with latest version of terraform

--- main.tf
+++ main.tf
@@ -40,20 +40,15 @@ module "blog_alb" {
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
-  target_groups = [
-    {
+  target_groups = {
+    ex-instance = {
       name_prefix      = "blog"
-      backend_protocol = "HTTP"
-      backend_port     = 8080
+      protocol = "HTTP"
+      port     = 80
       target_type      = "instance"
-      targets = [
-        {
-          target_id = aws_instance.blog.id
-          port = 8080
-        }
-      ]
+      target_id = aws_instance.blog.id
     }
-  ]
+  }
 
   listeners = {
     ex-http-https-redirect = {
