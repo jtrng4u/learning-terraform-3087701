d37df30bd6980164b5f5e112df492c17f172138d
jtrng4u <jtrng4u@gmail.com>
2025-03-01 22:07:14 -0600

    Cleared the earlier SG to utilized SG module.

--- main.tf
+++ main.tf
@@ -34,53 +34,11 @@ module "blog_sg" {
 
   vpc_id = data.aws_vpc.default.id
 
-  ingress_rules = ["http-8080-tcp"]
+  ingress_rules = ["http-8080-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
   name = "blog"
   description = "Security group for blog"
-}
-
-resource "aws_security_group" "blog" {
-  name = "blog"
-  vpc_id = data.aws_vpc.default.id
-  tags = {
-    name = "blog"
-  }
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-
-  type = "ingress"
-  to_port = "8080"
-  from_port = "8080"
-  protocol = "tcp"
-  cidr_blocks = [
-    "0.0.0.0/0"
-  ]
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type      = "ingress"
-  from_port = "443"
-  to_port   = "443"
-  protocol  = "tcp"
-  cidr_blocks = [
-    "0.0.0.0/0"
-  ]
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type      = "egress"
-  from_port = 0
-  to_port   = 0
-  protocol  = "-1"
-  cidr_blocks = [
-    "0.0.0.0/0"
-  ]
-  security_group_id = aws_security_group.blog.id
 }
