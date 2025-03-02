300e149eb2d823bcd527533c44d5140fab80d654
jtrng4u <jtrng4u@gmail.com>
2025-03-01 22:00:24 -0600

    Added new module for security group.

--- main.tf
+++ main.tf
@@ -22,10 +22,25 @@ data "aws_vpc" "default" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
   tags = {
     Name = "Learning Terraform"
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingegress_rules = ["http-8080-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  name = "blog"
+  description = "Security group for blog"
 }
 
 resource "aws_security_group" "blog" {
