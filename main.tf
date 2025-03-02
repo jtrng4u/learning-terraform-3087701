32cd28b69d451e2d360380a6efda512ed067abe4
jtrng4u <jtrng4u@gmail.com>
2025-03-02 09:04:56 -0600

    created vpc , alb and target groups

--- main.tf
+++ main.tf
@@ -14,10 +14,75 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+module "blog_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "blog_vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = ["us-east-1", "us-west-2"]
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  tags = {
+    Name = "blog_vpc"
+    Terraform = "true"
+  }
 }
 
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "blog"
+      backend_protocol = "HTTP"
+      backend_port     = 8080
+      target_type      = "instance"
+      targets = [
+        {
+          target_id = aws_instance.blog.id
+          port = 8080
+        }
+      ]
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Terraform = "true"
+  }
+}
+
+module "blog_asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+
+  name = "blog-asg"
+
+  min_size                  = 1
+  max_size                  = 2
+  health_check_type         = "ELB"
+  vpc_zone_identifier       = module.blog_vpc.public_subnets
+  target_group_arns         = module.blog_alb.target_group_arns
+
+  tags = {
+    Terraform = "true"
+  }
+}
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
@@ -32,7 +97,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules = ["http-8080-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
